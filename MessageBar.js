a60dd0248e5d0a2691ec7bc7024a8d613e26712a
Brian Doyle <brian@tf83.com>
2016-12-25 06:55:49 -0800

    Blue bar fix

--- MessageBar.js
+++ MessageBar.js
@@ -35,7 +35,9 @@ class MessageBar extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setNewState(nextProps);
+    if (nextProps && Object.keys(nextProps).length > 0) {
+      this.setNewState(nextProps);
+    }
   }
 
   setNewState(state) {
